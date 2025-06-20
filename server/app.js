a79c608b8e355323df45d7e324769593e4d54803
raftouch <rafanelkin@gmail.com>
2025-06-20 12:37:58 +0200

    app up and running

--- server/app.js
+++ server/app.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Init");
+});
+
+const start = async () => {
+  try {
+    app.listen(port, () => console.log(`App listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
