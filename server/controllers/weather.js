da074ee13f6d6e7db7129b98229bc9cade0203ce
raftouch <rafanelkin@gmail.com>
2025-06-20 13:27:19 +0200

    :recycle: refactor - cleaner architecture - move get weather to controller

---
+++ server/controllers/weather.js
@@ -0,0 +1,15 @@
+const getWeather = async (req, res) => {
+  const apiKey = process.env.API_KEY;
+  const city = "paris";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { getWeather };
--- server/routes/weather.js
+++ server/routes/weather.js
@@ -1,18 +1,7 @@
 const express = require("express");
+const { getWeather } = require("../controllers/weather");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const apiKey = process.env.API_KEY;
-  const city = "paris";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    res.send(data);
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.get("/", getWeather);
 
 module.exports = router;
