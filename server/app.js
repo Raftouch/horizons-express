4fa7ef92c2fcffc9ad97bdb552435921796838e2
raftouch <rafanelkin@gmail.com>
2025-06-20 14:32:07 +0200

    :wrench: add cors config

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use("/", require("./routes/weather"));
 
 const start = async () => {
