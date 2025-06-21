350b96d618f8505245196018e03e43f04aaf9e9f
raftouch <rafanelkin@gmail.com>
2025-06-21 13:47:26 +0200

    :bento: add weather model

---
+++ client/src/models/weather.ts
@@ -0,0 +1,25 @@
+export interface Weather {
+  weather: [
+    {
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    humidity: number;
+  };
+  wind: {
+    speed?: number;
+  };
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  name: string;
+}
