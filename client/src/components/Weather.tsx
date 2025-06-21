4689f367d31d240cf55c287ba95ef0446ea484c8
raftouch <rafanelkin@gmail.com>
2025-06-21 14:49:10 +0200

    :art: improve structure - move mapping to utils

--- client/src/components/Weather.tsx
+++ client/src/components/Weather.tsx
@@ -1,31 +1,11 @@
 import { useState } from "react";
 import { API_URL } from "../utils/api";
 import type { Weather } from "../models/weather";
+import { iconMapping } from "../utils/icon-mapping";
 
 export default function Weather() {
   const [city, setCity] = useState<string>("");
   const [cityData, setCityData] = useState<Weather | null>(null);
-
-  const iconMapping: Record<string, string> = {
-    "01d": "wi-day-sunny",
-    "01n": "wi-night-clear",
-    "02d": "wi-day-cloudy",
-    "02n": "wi-night-alt-cloudy",
-    "03d": "wi-cloud",
-    "03n": "wi-cloud",
-    "04d": "wi-cloudy",
-    "04n": "wi-cloudy",
-    "09d": "wi-showers",
-    "09n": "wi-showers",
-    "10d": "wi-day-rain",
-    "10n": "wi-night-alt-rain",
-    "11d": "wi-thunderstorm",
-    "11n": "wi-thunderstorm",
-    "13d": "wi-snow",
-    "13n": "wi-snow",
-    "50d": "wi-fog",
-    "50n": "wi-fog",
-  };
 
   const iconCode = cityData?.weather[0].icon;
   const iconClass = iconCode ? iconMapping[iconCode] : "";
