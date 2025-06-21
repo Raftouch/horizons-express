613c6afaff6c20395e1f900a7f63f2da9d2ab123
raftouch <rafanelkin@gmail.com>
2025-06-21 14:46:16 +0200

    :sparkles: add mapping to display weather icons

--- client/src/components/Weather.tsx
+++ client/src/components/Weather.tsx
@@ -6,6 +6,30 @@ export default function Weather() {
   const [city, setCity] = useState<string>("");
   const [cityData, setCityData] = useState<Weather | null>(null);
 
+  const iconMapping: Record<string, string> = {
+    "01d": "wi-day-sunny",
+    "01n": "wi-night-clear",
+    "02d": "wi-day-cloudy",
+    "02n": "wi-night-alt-cloudy",
+    "03d": "wi-cloud",
+    "03n": "wi-cloud",
+    "04d": "wi-cloudy",
+    "04n": "wi-cloudy",
+    "09d": "wi-showers",
+    "09n": "wi-showers",
+    "10d": "wi-day-rain",
+    "10n": "wi-night-alt-rain",
+    "11d": "wi-thunderstorm",
+    "11n": "wi-thunderstorm",
+    "13d": "wi-snow",
+    "13n": "wi-snow",
+    "50d": "wi-fog",
+    "50n": "wi-fog",
+  };
+
+  const iconCode = cityData?.weather[0].icon;
+  const iconClass = iconCode ? iconMapping[iconCode] : "";
+
   const fetchWeather = async (city: string) => {
     if (!city) return;
 
@@ -46,6 +70,7 @@ export default function Weather() {
           <h1>{cityData.name}</h1>
           <p>{Math.round(cityData.main.temp)}</p>
           <p>{cityData.weather[0].description}</p>
+          <i className={`wi ${iconClass}`}></i>
         </div>
       ) : null}
     </>
