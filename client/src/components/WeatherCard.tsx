505e67512c4e584055cf163aa5f2e654721f9d5c
raftouch <rafanelkin@gmail.com>
2025-06-26 14:55:39 +0200

    :building_construction: restructure code - move 'add to list' button inside weather card

--- client/src/components/WeatherCard.tsx
+++ client/src/components/WeatherCard.tsx
@@ -1,12 +1,19 @@
 import type { Weather } from "../models/weather";
 import { iconMapping } from "../utils/mapping";
 import { formatTime } from "../utils/format";
+import { useDispatch } from "react-redux";
+import type { AddDispatch } from "../store/store";
+import { addWeather } from "../store/weather-slice";
 
 interface CityWeatherProps {
   cityWeather: Weather;
+  showAddButton: Boolean;
 }
 
-export default function WeatherCard({ cityWeather }: CityWeatherProps) {
+export default function WeatherCard({
+  cityWeather,
+  showAddButton = false,
+}: CityWeatherProps) {
   const iconCode = cityWeather?.weather[0].icon;
   const iconClass = iconCode ? iconMapping[iconCode] : "";
 
@@ -23,8 +30,14 @@ export default function WeatherCard({ cityWeather }: CityWeatherProps) {
     ? Math.round(cityWeather.wind.speed * 3.6)
     : null;
 
+  const dispatch = useDispatch<AddDispatch>();
+
+  const handleClick = () => {
+    if (cityWeather) dispatch(addWeather(cityWeather));
+  };
+
   return (
-    <div className="flex flex-col w-[364px] py-10 bg-white rounded rounded-xl items-center">
+    <div className="flex flex-col w-[364px] p-8 bg-white rounded rounded-xl items-center">
       <div className="text-center">
         <h1 className="text-2xl font-bold">
           {cityWeather.name}, {cityWeather.sys.country}
@@ -68,6 +81,16 @@ export default function WeatherCard({ cityWeather }: CityWeatherProps) {
           <p>Wind: {windSpeed} km/h</p>
         </div>
       </div>
+      {showAddButton ? (
+        <button
+          onClick={handleClick}
+          className={`mt-10 text-white w-full px-2 py-1 rounded-full cursor-pointer bg-green-600 hover:bg-green-700 ${
+            !cityWeather ? "opacity-50 cursos-not-allowed" : ""
+          }`}
+        >
+          Add to my list
+        </button>
+      ) : null}
     </div>
   );
 }
