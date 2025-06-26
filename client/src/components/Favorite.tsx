3756a6f294768287582f813bee68c19890e7fb06
raftouch <rafanelkin@gmail.com>
2025-06-26 14:57:10 +0200

    :zap: update fav list card not to show add button

--- client/src/components/Favorite.tsx
+++ client/src/components/Favorite.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../store/store";
-import WeatherCard from "./Weathercard";
+import WeatherCard from "./WeatherCard";
 
 export default function Favorite() {
   const cities = useSelector((state: RootState) => state.cities.weather);
@@ -8,7 +8,7 @@ export default function Favorite() {
   return (
     <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-10 xl:grid-cols-3 xl:gap-24">
       {cities.map((city) => (
-        <WeatherCard key={city.id} cityWeather={city} />
+        <WeatherCard key={city.id} cityWeather={city} showAddButton={false} />
       ))}
     </div>
   );
