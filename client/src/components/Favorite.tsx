2841cd71d0596af2e289b25e20704e7785cc86f8
raftouch <rafanelkin@gmail.com>
2025-06-26 16:47:13 +0200

    :zap: add message when no cities in fav list

--- client/src/components/Favorite.tsx
+++ client/src/components/Favorite.tsx
@@ -8,11 +8,15 @@ export default function Favorite() {
   return (
     <div className="mt-5">
       <h1 className="text-white text-center text-2xl my-10">Favorite Cities</h1>
-      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-10 xl:grid-cols-3 xl:gap-24">
-        {cities.map((city) => (
-          <WeatherCard key={city.id} cityWeather={city} />
-        ))}
-      </div>
+      {cities.length > 0 ? (
+        <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-10 xl:grid-cols-3 xl:gap-24">
+          {cities.map((city) => (
+            <WeatherCard key={city.id} cityWeather={city} />
+          ))}
+        </div>
+      ) : (
+        <div className="text-slate-400">No cities in your list yet...</div>
+      )}
     </div>
   );
 }
