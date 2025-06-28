cf426f9136a54929098a89348aaea675e5e7d235
raftouch <rafanelkin@gmail.com>
2025-06-28 13:19:35 +0200

    :fire: rm dead code

--- client/src/components/Weather.tsx
+++ client/src/components/Weather.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-// import { API_URL } from "../utils/api";
 import type { Weather } from "../models/weather";
 import { FaSearch } from "react-icons/fa";
 import Favorite from "./Favorite";
@@ -10,40 +9,12 @@ import { fetchWeather } from "../store/weather-slice";
 
 export default function Weather() {
   const [city, setCity] = useState<string>("");
-  // const [cityData, setCityData] = useState<Weather | null>(null);
-  // const [error, setError] = useState<string | null>(null);
-  // const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const { selectedCityWeather, isLoading, error } = useSelector(
     (state: RootState) => state.cities
   );
+
   const dispatch = useDispatch<AddDispatch>();
-
-  // const fetchWeather = async (city: string) => {
-  //   if (!city) return;
-
-  //   try {
-  //     setIsLoading(true);
-  //     setError(null);
-  //     const response = await fetch(`${API_URL}/?city=${city}`);
-  //     console.log("res : ", response);
-  //     const data = await response.json();
-  //     console.log("data : ", data);
-
-  //     if (data.cod === "404") {
-  //       setCityData(null);
-  //       setError(data.message);
-  //     } else {
-  //       setCityData(data);
-  //     }
-  //   } catch (error) {
-  //     setCityData(null);
-  //     setError("An error occured while fetching data");
-  //     console.error("Error fetching weather", error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCity(e.target.value);
