8f4c8eb4cd60a0124adb65e7a17ac751d881a775
raftouch <rafanelkin@gmail.com>
2025-07-04 20:08:01 +0200

    Reapply ":fire: rm comments"
    This reverts commit 237e6cf47f7e0aa26f4ceb70581f2bc51e93d467.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
-// import NavBar from "./components/NavBar";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div className="font-mono bg-slate-900">
-      {/* <NavBar /> */}
       <MainPage />
     </div>
   );
