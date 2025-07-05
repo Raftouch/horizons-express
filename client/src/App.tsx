15ff2a57e637ec0720c5059a338a9f652167eeef
raftouch <rafanelkin@gmail.com>
2025-07-05 14:25:49 +0200

    :lipstick: update css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div className="font-mono bg-gradient-to-r from-indigo-600 from-10% via-sky-600 via-50% to-purple-600 to-90%">
+    <div className="font-mono bg-gradient-to-r from-slate-900 to-indigo-600">
       <MainPage />
     </div>
   );
