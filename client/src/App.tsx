e7ab4e09b10368aa1d6ac1cea409d1070a82bad5
raftouch <rafanelkin@gmail.com>
2025-07-05 14:34:01 +0200

    :lipstick: update css - fix white bg on iphone5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div className="font-mono bg-gradient-to-r from-slate-900 to-indigo-600">
+    <div className="font-mono">
       <MainPage />
     </div>
   );
