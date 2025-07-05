3ead04124d60e99d7c42538ec068b7ca76c3d567
raftouch <rafanelkin@gmail.com>
2025-07-05 12:44:02 +0200

    :ambulance: fix client production url to allow cors

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://horizons-express.netlify.app/"],
+    origin: ["http://localhost:5173", "https://horizons-express.netlify.app"],
   })
 );
 app.use("/", require("./routes/weather"));
