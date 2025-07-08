c83ac12b1edf0eff6e18ddf98c000006a773e412
raftouch <rafanelkin@gmail.com>
2025-07-08 13:23:57 +0200

    :bento: update translations

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -14,17 +14,59 @@ i18n
     resources: {
       en: {
         translation: {
-          humidity: "Humidity",
+          weather: {
+            humidity: "Humidity",
+            sunrise: "Sunrise",
+            sunset: "Sunset",
+            wind: "Wind",
+            feelsLike: "Feels like",
+            kmH: "km/h",
+            min: "Min",
+            max: "Max",
+          },
+          actions: {
+            addToList: "Add to my list",
+            removeFromList: "Remove from list",
+            search: "Search for a city",
+          },
         },
       },
       fr: {
         translation: {
-          humidity: "Humidité",
+          weather: {
+            humidity: "Humidité",
+            sunrise: "Lever du soleil",
+            sunset: "Coucher du soleil",
+            wind: "Vent",
+            feelsLike: "Ressenti",
+            kmH: "km/h",
+            min: "Min",
+            max: "Max",
+          },
+          actions: {
+            addToList: "Ajouter à ma liste",
+            removeFromList: "Retirer de ma liste",
+            search: "Rechercher une ville",
+          },
         },
       },
       ru: {
         translation: {
-          humidity: "Влажность",
+          weather: {
+            humidity: "Влажность",
+            sunrise: "Восход солнца",
+            sunset: "Закат солнца",
+            wind: "Ветер",
+            feelsLike: "Ощущается как",
+            kmH: "км/ч",
+            min: "Мин.",
+            max: "Макс.",
+          },
+          actions: {
+            addToList: "Добавить в список",
+            removeFromList: "Убрать из списка",
+            search: "Поиск города",
+          },
         },
       },
     },
