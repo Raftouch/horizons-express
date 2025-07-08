1a0514979eb563b276eb5dacf6141374e38d845d
raftouch <rafanelkin@gmail.com>
2025-07-08 14:27:03 +0200

    :bento: add weather main description translations mapping

---
+++ client/src/utils/mappings/description.ts
@@ -0,0 +1,80 @@
+export const weatherMainTranslations: Record<
+  string,
+  { "en-US": string; "fr-FR": string; "ru-RU": string }
+> = {
+  Thunderstorm: {
+    "en-US": "Thunderstorm",
+    "fr-FR": "Orage",
+    "ru-RU": "Гроза",
+  },
+  Drizzle: {
+    "en-US": "Drizzle",
+    "fr-FR": "Bruine",
+    "ru-RU": "Морось",
+  },
+  Rain: {
+    "en-US": "Rain",
+    "fr-FR": "Pluie",
+    "ru-RU": "Дождь",
+  },
+  Snow: {
+    "en-US": "Snow",
+    "fr-FR": "Neige",
+    "ru-RU": "Снег",
+  },
+  Clear: {
+    "en-US": "Clear",
+    "fr-FR": "Dégagé",
+    "ru-RU": "Ясно",
+  },
+  Clouds: {
+    "en-US": "Clouds",
+    "fr-FR": "Nuageux",
+    "ru-RU": "Облачно",
+  },
+  Mist: {
+    "en-US": "Mist",
+    "fr-FR": "Brume",
+    "ru-RU": "Туман",
+  },
+  Smoke: {
+    "en-US": "Smoke",
+    "fr-FR": "Fumée",
+    "ru-RU": "Дым",
+  },
+  Haze: {
+    "en-US": "Haze",
+    "fr-FR": "Brume sèche",
+    "ru-RU": "Мгла",
+  },
+  Dust: {
+    "en-US": "Dust",
+    "fr-FR": "Poussière",
+    "ru-RU": "Пыль",
+  },
+  Fog: {
+    "en-US": "Fog",
+    "fr-FR": "Brouillard",
+    "ru-RU": "Туман",
+  },
+  Sand: {
+    "en-US": "Sand",
+    "fr-FR": "Sable",
+    "ru-RU": "Песок",
+  },
+  Ash: {
+    "en-US": "Ash",
+    "fr-FR": "Cendres",
+    "ru-RU": "Пепел",
+  },
+  Squall: {
+    "en-US": "Squall",
+    "fr-FR": "Rafale",
+    "ru-RU": "Шквал",
+  },
+  Tornado: {
+    "en-US": "Tornado",
+    "fr-FR": "Tornade",
+    "ru-RU": "Торнадо",
+  },
+};
