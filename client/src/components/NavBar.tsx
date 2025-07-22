831f336ab2ac07ae75b299fb102af4dce935a2ad
raftouch <rafanelkin@gmail.com>
2025-07-22 19:21:33 +0200

    :lipstick: move title and logo from main page to navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
+import { GiStripedSun } from "react-icons/gi";
+import { useTranslation } from "react-i18next";
 
 export default function NavBar() {
+  const { t } = useTranslation();
+
   return (
     <div className="flex justify-between text-white px-4 py-2">
-      <span>LOGO</span>
+      <div className="flex gap-5">
+        <h1 className="text-white mb-2">{t("appTitle")}</h1>
+        <GiStripedSun className="text-white w-5 h-5" />
+      </div>
       <ul className="flex gap-8">
         <li>
           <Link to="/">Current Weather</Link>
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,17 +1,9 @@
-import { useTranslation } from "react-i18next";
 import Favorite from "../components/Favorite";
 import Weather from "../components/Weather";
-import { GiStripedSun } from "react-icons/gi";
 
 export default function MainPage() {
-  const { t } = useTranslation();
-
   return (
     <div className="px-10 py-8 flex flex-col items-center text-slate-900 w-full min-h-screen">
-      <div className="flex gap-5">
-        <h1 className="text-white text-xl mb-2">{t("appTitle")}</h1>
-        <GiStripedSun className="text-white w-5 h-5" />
-      </div>
       <Weather />
       <Favorite />
     </div>
