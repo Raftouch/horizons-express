d07da3ddc2a5f36314d6516b6e8e4171f543e445
raftouch <rafanelkin@gmail.com>
2025-07-22 19:10:25 +0200

    :zap: move navbar and footer to layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div className="font-mono">
-      <NavBar />
       <MainPage />
-      <Footer />
     </div>
   );
 }
