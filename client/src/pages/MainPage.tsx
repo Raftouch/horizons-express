b98e5e20d0780116eecc05f3bb7ed6046f263b96
raftouch <rafanelkin@gmail.com>
2025-07-24 17:58:44 +0200

    :adhesive_bandage: fix css

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -3,8 +3,7 @@ import Weather from "../components/Weather";
 
 export default function MainPage() {
   return (
-    // <div className="px-10 py-8 flex flex-col items-center text-slate-900 w-full min-h-screen">
-    <div className="pt-20">
+    <div className="px-10 pb-8 pt-20 flex flex-col items-center text-slate-900 w-full min-h-screen">
       <Weather />
       <Favorite />
     </div>
