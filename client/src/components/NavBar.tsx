c60bdfe4d8664a473e3a3835ae78b4597a542ddc
raftouch <rafanelkin@gmail.com>
2025-07-24 17:21:23 +0200

    :lipstick: update navbar css

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { GiStripedSun } from "react-icons/gi";
 import { useTranslation } from "react-i18next";
 
 export default function NavBar() {
   const { t } = useTranslation();
+  const location = useLocation();
 
   return (
     <div className="fixed w-full flex justify-between text-white px-4 py-2 h-16 items-center bg-[linear-gradient(to_right,_#0f172a,_#4f46e5)]">
@@ -12,10 +13,18 @@ export default function NavBar() {
         <GiStripedSun className="text-white w-5 h-5" />
       </div>
       <ul className="flex gap-8">
-        <li>
+        <li
+          className={`hover:text-rose-500 ${
+            location.pathname === "/" ? "border-b" : ""
+          }`}
+        >
           <Link to="/">Current Weather</Link>
         </li>
-        <li>
+        <li
+          className={`hover:text-rose-500 ${
+            location.pathname === "/forecast" ? "border-b" : ""
+          }`}
+        >
           <Link to="/forecast">Forecast</Link>
         </li>
       </ul>
